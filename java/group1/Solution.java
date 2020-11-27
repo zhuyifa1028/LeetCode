ae7f7749041fade849c2a3fc3a3181ffcfa942f2
夜语丶 <zhuyifa1028@gmail.com>
2020-11-27 22:21:57 +0800

    【修改】1-100题解移到第一分组

--- java/group1/Solution.java
+++ java/group1/Solution.java
@@ -1,4 +1,4 @@
-// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+package group1;// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
@@ -14,6 +14,8 @@
 // 链接：https://leetcode-cn.com/problems/two-sum
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+import common.Array;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +29,7 @@ import java.util.Map;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] ints = ArrayUtils.randomInt(20, 10);
+        int[] ints = Array.randomInt(20, 10);
         System.out.println("输入：" + Arrays.toString(ints) + " " + 20 + "");
         System.out.println("输出：" + Arrays.toString(twoSum(ints, 20)));
     }
--- java/group1/Solution28.java
+++ java/group1/Solution28.java
@@ -1,4 +1,4 @@
-// 实现 strStr() https://baike.baidu.com/item/strstr/811469 函数。
+package group1;// 实现 strStr() https://baike.baidu.com/item/strstr/811469 函数。
 // 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
 // 如果不存在，则返回 -1。
 //
