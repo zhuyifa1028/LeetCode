d1620ecf3b73ce966d35f1390f4e1544123bb08d
夜语丶 <zhuyifa1028@gmail.com>
2020-11-28 11:01:19 +0800

    【修改】题目布局

--- java/group1/Solution1.java
+++ java/group1/Solution1.java
@@ -1,4 +1,6 @@
-package group1;// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+package group1;
+
+// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
@@ -26,7 +28,7 @@ import java.util.Map;
  * @author zhuyifa
  * @version 2020-11-12
  */
-public class Solution {
+public class Solution1 {
 
     public static void main(String[] args) {
         int[] ints = Array.randomInt(20, 10);
--- java/group1/Solution13.java
+++ java/group1/Solution13.java
@@ -1,4 +1,6 @@
-package group1;// 罗马数字包含以下七种字符: I，V，X，L，C，D 和 M。
+package group1;
+
+// 罗马数字包含以下七种字符: I，V，X，L，C，D 和 M。
 //
 //   字符          数值
 //   I             1
